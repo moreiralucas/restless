a75ad6533958a91be67db2d42b58a3dbbed12925
Lucas Moreira <lucas.pires-ext@intelipost.com.br>
2024-07-12 00:07:27 -0300

    feat: update django compatibility

--- examples/django/posts/urls.py
+++ examples/django/posts/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url, include
+from django.urls import path, include
 
 from .api import PostResource
 
 
 urlpatterns = [
-    url(r'^posts/', include(PostResource.urls())),
+    path('posts/', include(PostResource.urls())),
 
     # Alternatively, if you don't like the defaults...
     # url(r'^posts/$', PostResource.as_list(), name='api_posts_list'),
